41fcd161295c969b4ad86fab714b61f956d5a8e5
Matthew Davies <70969486+mfdavies@users.noreply.github.com>
2024-01-14 12:27:18 -0700

    Refactor Files to make the folders make more sense

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Patient login email content
+EMAIL_SUBJECT = "MobilityMate Account Access"
+EMAIL_BODY_TEMPLATE = """Dear {name},
+
+Welcome to MobilityMate - your dedicated partner in staying active and healthy! 🌟 
+
+To access your personalized exercise account and stay up to date with exercises recommended by your practitioner, simply click on the link below:
+
+🔗 https://mobilitymate-a8b53.web.app/{practitionId}/patient/{patientId}
+
+Your well-being is our priority! If you have any questions or need support, our team is here to assist you every step of the way.
+
+Stay active, stay healthy!
+The MobilityMate Team 🏋️‍♂️
+"""
